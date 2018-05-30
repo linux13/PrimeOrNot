9270555a9a78139d12fc4bea740d642ae38b1c89
linux13 <asfaklus2@gmail.com>
2018-05-30 00:14:56 -0700

    Add menu

--- app/src/main/java/com/primeornot/Act1.java
+++ app/src/main/java/com/primeornot/Act1.java
@@ -1,12 +1,15 @@
 package com.primeornot;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.math.BigInteger;
 import java.util.Arrays;
@@ -21,21 +24,26 @@ public class Act1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act1);
+        android.support.v7.app.ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setHomeButtonEnabled(true);
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
 
         ed = (EditText) findViewById(R.id.edit);
         tx = (TextView) findViewById(R.id.txt);
         bt = (Button) findViewById(R.id.btn);
         //
 
-       // prime();
+        // prime();
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String s1 = ed.getText().toString();
                 Long n = Long.parseLong(s1);
                 BigInteger bg = new BigInteger(String.valueOf(n));
-                boolean t=bg.isProbablePrime(1);
-                if (t==true) tx.setText(n + " is Prime");
+                boolean t = bg.isProbablePrime(1);
+                if (t == true) tx.setText(n + " is Prime");
                 else tx.setText(n + " is Composite");
             }
         });
@@ -59,8 +67,8 @@ public class Act1 extends AppCompatActivity {
 
             if (arr[i] == 1) {
 
-                for (int j = i*i; j <= 100005; j+=i) {
-                  //  arr[j] = 0;
+                for (int j = i * i; j <= 100005; j += i) {
+                    //  arr[j] = 0;
                 }
 
             }
@@ -68,5 +76,28 @@ public class Act1 extends AppCompatActivity {
         }
 
     }
-}
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            Toast.makeText(this, "Pressed about", Toast.LENGTH_SHORT).show();
+            //return  true;
+            onBackPressed();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+        @Override
+        public void onBackPressed () {
+            //super.onBackPressed();
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+    }
+
+
+
 
--- app/src/main/java/com/primeornot/Act2.java
+++ app/src/main/java/com/primeornot/Act2.java
@@ -1,10 +1,17 @@
 package com.primeornot;
 
+import android.app.ActionBar;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuWrapperFactory;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Act2 extends AppCompatActivity {
     Button button;
@@ -13,6 +20,13 @@ public class Act2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act2);
+        android.support.v7.app.ActionBar actionbar = getSupportActionBar();
+        if(actionbar!=null) {
+            actionbar.setHomeButtonEnabled(true);
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
+
+
         button = findViewById(R.id.button);
         progress= new ProgressDialog(this);
         button.setOnClickListener(new View.OnClickListener() {
@@ -23,4 +37,38 @@ public class Act2 extends AppCompatActivity {
             }
         });
     }
+
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return true;
+}
+        public boolean onOptionsItemSelected(MenuItem item){
+               int id=item.getItemId();
+               if(id==R.id.about){
+                   Toast.makeText(this, "Pressed about", Toast.LENGTH_SHORT).show();
+                   //return  true;
+
+               }
+
+           else  if(id==R.id.logout){
+                Toast.makeText(this, "Pressed logout", Toast.LENGTH_SHORT).show();
+                //return  true;
+
+            }
+            else if(id==R.id.test){
+
+                   Toast.makeText(this, "Pressed test", Toast.LENGTH_SHORT).show();
+               }
+               else if(id==android.R.id.home){
+                   Intent intent=new Intent(Act2.this,Act1.class);
+                   startActivity(intent);
+
+               }
+
+
+                return  true;
+        }
+
 }
